35b04670d8c35a05a684e60482ce11ebad42dfc1
carlosylasalud2020 <carlosylasalud@yahoo.com.ar>
2022-03-22 12:55:14 -0500

    menu 2

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -6,41 +6,68 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './home.component.html',
   styleUrls: ['home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit, OnDestroy, OnInit {
   display: boolean = false
   items: MenuItem[] = [];
 
-  constructor() { }
+  menuMode = 'static'
+
+  documentClickListener: () => void;
+
+  constructor() { }  
 
   ngOnInit() {
     this.items = [
       {
         label: 'Archivo',
         icon: 'pi pi-pw pi-file',
+        expanded:true,
         items: [{
-          label: 'Articulo',
-          icon: 'pi pi-fw pi-plus',
-          items: [
-            { label: 'User', icon: 'pi pi-fw pi-user-plus' },
-            { label: 'Filter', icon: 'pi pi-fw pi-filter' }
-          ]
+          label: 'Lineas',
+          icon: 'pi pi-fw pi-plus',          
         },
-        { label: 'Open', icon: 'pi pi-fw pi-external-link' },
+        { label: 'Proveedores', icon: 'pi pi-fw pi-external-link' },
         { separator: true },
-        { label: 'Quit', icon: 'pi pi-fw pi-times' }
+        { label: 'Articulos', icon: 'pi pi-fw pi-times' },
+        { label: 'Clientes', icon: 'pi pi-fw pi-user' },
+        { label: 'Vendedores', icon: 'pi pi-fw pi-times' },
+        { label: 'Bancos', icon: 'pi pi-fw pi-times' },
+        { label: 'Cuentas', icon: 'pi pi-fw pi-times' },
+        { label: 'Grupos de Conceptos', icon: 'pi pi-fw pi-times' },
+        { label: 'Almacenes', icon: 'pi pi-fw pi-times' },
+        { label: 'Tipo de Persona', icon: 'pi pi-fw pi-times' },
         ]
       },
       {
-        label: 'Edit',
+        label: 'Transacciones',
         icon: 'pi pi-fw pi-pencil',
+        expanded:true,
         items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' }
+          { label: 'Articulos', 
+            icon: 'pi pi-fw pi-trash',
+            items:[
+              {
+                label:'Compras o Ingresos',
+                icon: 'pi pi-fw pi-trash',
+              },
+              {
+                label:'Salidas o Ventas',
+                icon: 'pi pi-fw pi-trash',
+              },
+              {
+                label:'Proformas',
+                icon: 'pi pi-fw pi-trash',
+              }
+            ] 
+          },
+          { label: 'Clientes', icon: 'pi pi-fw pi-refresh' },
+          { label: 'Proveedores', icon: 'pi pi-fw pi-refresh' }
         ]
       },
       {
         label: 'Help',
         icon: 'pi pi-fw pi-question',
+        expanded:true,
         items: [
           {
             label: 'Contents',
@@ -69,6 +96,7 @@ export class HomeComponent implements OnInit {
       {
         label: 'Actions',
         icon: 'pi pi-fw pi-cog',
+        expanded:true,
         items: [
           {
             label: 'Edit',
@@ -90,4 +118,62 @@ export class HomeComponent implements OnInit {
     ];
   }
 
+  ngAfterViewInit() {
+    // hides the overlay menu and top menu if outside is clicked
+    this.documentClickListener = this.renderer.listen('body', 'click', (event) => {
+        if (!this.isDesktop()) {
+            if (!this.menuClick) {
+                this.menuActiveMobile = false;
+            }
+
+            if (!this.topMenuButtonClick) {
+                this.hideTopMenu();
+            }
+        }
+        else {
+            if (!this.menuClick && this.isOverlay()) {
+                this.menuInactiveDesktop = true;
+            }
+            if (!this.menuClick){
+                this.overlayMenuActive = false;
+            }
+        }
+
+        if (this.configActive && !this.configClick) {
+            this.configActive = false;
+        }
+
+        this.configClick = false;
+        this.menuClick = false;
+        this.topMenuButtonClick = false;
+    });
+}
+
+  toggleMenu(event: Event) {
+    this.menuClick = true;
+
+    if (this.isDesktop()) {
+        if (this.menuMode === 'overlay') {
+            if(this.menuActiveMobile === true) {
+                this.overlayMenuActive = true;
+            }
+
+            this.overlayMenuActive = !this.overlayMenuActive;
+            this.menuActiveMobile = false;
+        }
+        else if (this.app.menuMode === 'static') {
+            this.staticMenuInactive = !this.staticMenuInactive;
+        }
+    }
+    else {
+        this.menuActiveMobile = !this.menuActiveMobile;
+        this.topMenuActive = false;
+    }
+
+    event.preventDefault();
+  }
+
+  isDesktop() {
+    return window.innerWidth > 992;
+  }
 }
