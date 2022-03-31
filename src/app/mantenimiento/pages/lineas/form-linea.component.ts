9c6098e109bffb9c9e70190cc621343de24e56be
CGR <carlosylasalud@yahoo.com.ar>
2022-03-30 21:11:43 -0500

    todo 1

--- src/app/mantenimiento/pages/lineas/form-linea.component.ts
+++ src/app/mantenimiento/pages/lineas/form-linea.component.ts
@@ -3,12 +3,17 @@ import { Component,  EventEmitter,  Input, OnInit, Output } from '@angular/core'
 @Component({
   selector: 'app-form-linea',
   templateUrl: './form-linea.component.html',
-  styles: [
-  ]
+  styles: [`
+    :host ::ng-deep .p-dialog .p-dialog-header {
+      background-color:;
+    }
+  `]
 })
 export class FormLineaComponent implements OnInit {
   @Input() show:boolean = false
   @Output() showChange = new EventEmitter<boolean>()
+
+  titulo:string='Nueva Línea'
   
   
   constructor() { }
