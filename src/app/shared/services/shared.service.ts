21da36ebe0dc352e1454130b07f2e8104312ce1d
CGR <carlosylasalud@yahoo.com.ar>
2022-04-07 20:28:21 -0500

    updates to deploy to heroku

--- src/app/shared/services/shared.service.ts
+++ src/app/shared/services/shared.service.ts
@@ -5,7 +5,9 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class SharedService {
-  public apiUrl:BehaviorSubject<string> = new BehaviorSubject('http://127.0.0.1:8000/api')
+  //public apiUrl:BehaviorSubject<string> = new BehaviorSubject('http://127.0.0.1:8000/api')
+  public apiUrl:BehaviorSubject<string> = 
+    new BehaviorSubject('https://fast-mountain-03149.herokuapp.com/api')
 
   constructor() {}
   
